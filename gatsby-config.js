03ea2c56bbcd49539c1b7b313dd5d7649453fcea
Kresimir Cosic <kresimir.cosic.business@gmail.com>
2020-11-02 08:16:17 +0100

    Firebase default configuration Entered basic information with dummy data that needs to be changed for Firebase plugin to work.

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,20 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: "<YOUR_FIREBASE_API_KEY>",
+          authDomain: "<YOUR_FIREBASE_AUTH_DOMAIN>",
+          databaseURL: "<YOUR_FIREBASE_DATABASE_URL>",
+          projectId: "<YOUR_FIREBASE_PROJECT_ID>",
+          storageBucket: "<YOUR_FIREBASE_STORAGE_BUCKET>",
+          messagingSenderId: "<YOUR_FIREBASE_MESSAGING_SENDER_ID>",
+          appId: "<YOUR_FIREBASE_APP_ID>",
+        },
+      },
+    },
   ],
 }
